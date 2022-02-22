becfb0d2a2ef4b227747bfe46de6db371de1a640
Brandon <robertsbt@gmail.com>
2022-02-22 06:15:19 -0600

    initial commit

---
+++ app/appwrite.ts
@@ -0,0 +1,6 @@
+import { Appwrite } from 'appwrite';
+
+export const sdk = new Appwrite();
+
+
+export { sdk as api };
---
+++ app/routes/auth.tsx
@@ -0,0 +1,29 @@
+import { FormEvent } from 'react';
+import { LoaderFunction, useLoaderData } from 'remix';
+import { api } from '../appwrite';
+
+export const loader: LoaderFunction = () => {
+  return {
+    ENV: {
+      endpoint: process.env.APPWRITE_ENDPOINT,
+      projectId: process.env.APPWRITE_PROJECT_ID
+    }
+  };
+}
+
+export default function Auth() {
+  const data = useLoaderData();
+  api.setEndpoint(data.ENV.endpoint);
+  api.setProject(data.ENV.projectId);
+
+  function login(e: FormEvent<any>) {
+    e.preventDefault();
+    api.account.createOAuth2Session('google', 'http://localhost:3000/auth/callback', 'http://localhost:3000/auth');
+  }
+
+  return (
+    <div>
+      <button type="submit" onClick={login}>Login With Google</button>
+    </div>
+  );
+}
